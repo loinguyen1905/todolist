96f5f6f74aaf4536b01b6cd1a4dc6e0cf0d2aae7
Loiwibu2k4 <nguyenthangloi19052004@gmail.com>
2024-04-05 13:52:38 +0700

    1.0 basic app

---
+++ src/main/java/com/example/demo/models/User.java
@@ -0,0 +1,32 @@
+package com.example.demo.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "user")
+@Data
+public class User implements Serializable {
+    private static final long serialVersionUID = -297553281792804396L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Mapping thông tin biến với tên cột trong Database
+    @Column(name = "hp")
+    private int hp;
+    @Column(name = "stamina")
+    private int stamina;
+
+    // Nếu không đánh dấu @Column thì sẽ mapping tự động theo tên biến
+    private int atk;
+    private int def;
+    private int agi;
+}
